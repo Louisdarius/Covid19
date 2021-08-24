4b3105c020b84d114cde6eb91b70707b8829afc5
Louis_darius <louisdarius96@yahoo.fr>
2021-08-24 03:49:24 +0100

    add new host

--- models/dataBaseConnection.js
+++ models/dataBaseConnection.js
@@ -1,9 +1,20 @@
-const mongoose = require('mongoose');
-module.exports = mongoose.connect(process.env.MONGO_ATLAS_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("DATABASE CONNECTION OPENED!!!")
-    })
-    .catch(err => {
-        console.log(" OH OH THERE WAS AN ERROR CONNECTING TO THE DATABASE !!!!")
-        console.log(err)
-    });
+require("dotenv").config();
+const mongoose = require("mongoose");
+module.exports = mongoose
+  .connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("DATABASE CONNECTION OPENED!!!");
+  })
+  .catch((err) => {
+    console.log(" OH OH THERE WAS AN ERROR CONNECTING TO THE DATABASE !!!!");
+    console.log(err);
+  });
+
+//     const mongoose = require ("mongoose"),
+//   env = require ("./ configEnv");
+// module.exports = mongoose.connect (env.url, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: true,
+// });
